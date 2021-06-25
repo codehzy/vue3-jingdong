61d9dd3efa0dd9790e35c31ee85faa47c09f21ef
hzy <1257664828@qq.com>
2021-06-25 17:52:44 +0800

    搜索及banner

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
+  <!-- 首页顶部定位区域及搜索框 -->
   <div class="wrapper">
     <div class="position">
       <span class="iconfont position__icon">&#xe64f;</span>
       北京理工大学国防科技苑2号楼10层
       <span class="iconfont position__notice">&#xe6bb;</span>
     </div>
+    <div class="search">
+      <span class="iconfont">&#xe699;</span>
+      <input
+        placeholder="山姆会员优惠商品"
+        class="search__text search__text--focus"
+      />
+    </div>
+
+    <!-- 首页banner，做了CSSHick防抖 -->
+    <div class="banner">
+      <img
+        class="banner__img"
+        src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+        alt="banner"
+      />
+    </div>
+    <div>123</div>
   </div>
 
+  <!-- 首页底部导航 -->
   <div class="docker">
     <span class="docker__item docker__item--active">
       <div class="iconfont">&#xe60b;</div>
